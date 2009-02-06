c32a9bc8310cf80f9cd3e1ecac72e01d9f590d72
Pipelynx <Pipelynx@3e070776-f1fd-11dd-a295-0b7b0935bf94>
2009-02-06 20:52:34 +0000

    Authentication and retrieving of anime by ID is now possible

--- Nijikon/ADBConnection.h
+++ Nijikon/ADBConnection.h
@@ -6,12 +6,39 @@
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
+//#define AUTH [NSString stringWithString:@"AUTH user=%@&pass=%@&protover=%@&client=%@&clientver=%dnat=%d&enc=%@"
+
+#import "ADBResponseCodes.h"
+
 #import <Cocoa/Cocoa.h>
 
-#import <EDCommon/EDCommon.h>
+#import "NSFileHandle+Extensions.h"
+#import "EDUDPSocket.h"
+
+#import "ADBAnime.h"
+#import "ADBEpisode.h"
+#import "ADBFile.h"
+#import "ADBGroup.h"
 
 @interface ADBConnection : NSObject {
 	EDUDPSocket* socket;
+	NSMutableArray* log;
+	NSString* sessionKey;
 }
+void send(NSString* aString, EDSocket* socket, NSStringEncoding enc);
+NSString* receive(EDSocket* socket, NSStringEncoding enc);
 
+//Pre-query methods
+- (BOOL)connect:(unsigned short)port;
+- (BOOL)authenticate:(NSString*)username withPassword:(NSString*)password;
+
+//Query methods
+- (ADBAnime*)findAnimeByID:(NSString*)animeID;
+- (ADBAnime*)findAnimeByName:(NSString*)aName;
+
+//Post-query methods
+
+- (NSString*)sessionKey;
+- (NSString*)tailLog;
+- (NSArray*)log;
 @end
