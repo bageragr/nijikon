413c6880afab76b29f1e39cd929a2a4932c24d16
Pipelynx <Pipelynx@3e070776-f1fd-11dd-a295-0b7b0935bf94>
2009-07-12 13:36:09 +0000

    New command tool added to generate ed2k links for single files and all files in a directory (working except for a strange 505 INVALID INPUT)

---
+++ Nijikon/PLiPhoneController.m
@@ -0,0 +1,14 @@
+//
+//  PLiPhoneController.m
+//  Nijikon
+//
+//  Created by Pipelynx on 2/24/09.
+//  Copyright 2009 Martin Fellner. All rights reserved.
+//
+
+#import "PLiPhoneController.h"
+
+
+@implementation PLiPhoneController
+
+@end
